c1980d5c303f11f3d5bc3c9cc86610d4e07535d0
jprice <josh@outlawit.com>
2015-01-08 21:42:03 -0500

    updating

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInite338d7fb36a3c14f9fdffb52b594bed7::getLoader();
+return ComposerAutoloaderInita4d280b0afd72e0dd36d7d4137be3508::getLoader();
